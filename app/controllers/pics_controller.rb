132fb8c06bdd300b2524987aa3c68cf613b64210
Heather Parker <HParkerdesign@gmail.com>
2017-02-14 16:36:23 -0600

    Edit, Update and Destroy Actions

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,6 +3,7 @@ class PicsController < ApplicationController
 
 
 	def index
+		@pics = Pic.all.order("created_at DESC")
 
 	end
 
@@ -23,6 +24,23 @@ class PicsController < ApplicationController
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @pic.update(pic_params)
+			redirect_to @pic, notice: "Congrats! Pic was updated!"
+		else
+			render 'edit'
+		end
+
+	end
+
+	def destroy
+		@pic.destroy
+		redirect_to root_path
 	end
 
 	private
